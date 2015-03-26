2f9caf7144fac43683a195a003c1d3bc983b4ebc
Kevin Chen <erf@standarddoubt.com>
2015-03-25 21:53:40 -0400

    Initial Commit

---
+++ AnyCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnyCard
+//
+//  Created by Kevin Chen on 3/25/15.
+//  Copyright (c) 2015 Kevin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnyCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnyCard
+//
+//  Created by Kevin Chen on 3/25/15.
+//  Copyright (c) 2015 Kevin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
