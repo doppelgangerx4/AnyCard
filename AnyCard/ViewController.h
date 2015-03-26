90faa42a3e53de07ead0998d4558c647d03ae66e
Kevin Chen <erf@standarddoubt.com>
2015-03-26 02:09:56 -0400

    - initial implementation

--- AnyCard/ViewController.h
+++ AnyCard/ViewController.h
@@ -8,7 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+#define MAX_OPEN_DOCS 3
+
+@interface ViewController : UIViewController<UITextViewDelegate, UITextFieldDelegate>
+{
+    NSInteger m_linesCount;
+}
+
+@property (readwrite, atomic) NSMutableArray* inputStrings;
+@property (readwrite, atomic) NSString* outputString;
+@property (readwrite, atomic) NSInteger currentDocIndex;
+
+@property (weak, nonatomic) IBOutlet UITextView *inputTextView;
+@property (weak, nonatomic) IBOutlet UITextView *outputTextView;
+
+@property (weak, nonatomic) IBOutlet UILabel *lineCountLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *shuffleButton;
+@property (weak, nonatomic) IBOutlet UITextField *shuffleTextField;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *documentSegments;
+@property (weak, nonatomic) IBOutlet UIButton *resetDocumentButton;
 
 
 @end
--- AnyCard/ViewController.m
+++ AnyCard/ViewController.m
@@ -14,12 +14,102 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.inputStrings = [NSMutableArray arrayWithObjects:@"",@"",@"", nil];
+    
+    self.currentDocIndex = 0;
+    m_linesCount = [[self.lineCountLabel text] integerValue];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void) textViewDidChange:(UITextView *)textView
+{
+    [textView resignFirstResponder];
+}
+
+- (void) textViewDidEndEditing:(UITextView *)textView
+{
+    assert(self.currentDocIndex < [self.inputStrings count]);
+    
+    NSString* str = [textView text];
+    self.inputStrings[self.currentDocIndex] = str;
+    
+    m_linesCount = [[str componentsSeparatedByString:@"\n"] count];
+    [self.lineCountLabel setText:[NSString stringWithFormat:@"%lu",(unsigned long)m_linesCount]];
+}
+
+-(BOOL)textFieldShouldEndEditing:(UITextField *)textField
+{
+    NSInteger intVal = [[textField text] integerValue];
+    if (intVal < 1)
+    {
+        [textField setText:@"1"];
+    }
+    if ((intVal > m_linesCount) )
+    {
+        [textField setText:[NSString stringWithFormat:@"%lu",m_linesCount]];
+    }
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+- (IBAction)shuffleButtonTapped:(id)sender
+{
+    NSString* str = self.inputStrings[self.currentDocIndex];
+    if ([str length] > 0)
+    {
+        NSString* hand = [NSString string];
+
+        NSMutableArray* cards = [NSMutableArray arrayWithArray:[str componentsSeparatedByString:@"\n"]];
+        NSInteger draws = [[self.shuffleTextField text] integerValue];
+        
+        if (draws < 1)
+        {
+            NSLog(@"ERROR: Drawing an invalid number of cards.");
+            return;
+        }
+        
+        for (int i=0; i<draws; i++)
+        {
+            int index = arc4random_uniform((uint32_t)[cards count]);
+            
+            NSString* card = cards[index];
+            hand = [hand stringByAppendingFormat:@"%@\n",card];
+            [cards removeObjectAtIndex:index];
+        }
+        
+        [self.outputTextView setText:hand];
+    }
+}
+
+- (IBAction)resetTextViewButtonTapped:(id)sender
+{
+    self.inputStrings[self.currentDocIndex] = @"";
+    [self.inputTextView setText:@""];
+    [self.outputTextView setText:@""];
+}
+- (IBAction)segmentValueChanged:(UISegmentedControl *)sender
+{
+    [self setCurrentDocIndex:[sender selectedSegmentIndex]];
+    NSString* currentInputText = self.inputStrings[self.currentDocIndex];
+    [self.inputTextView setText:currentInputText];
+    [self.outputTextView setText:@""];
+    
+    m_linesCount = [[currentInputText componentsSeparatedByString:@"\n"] count];
+    [self.lineCountLabel setText:[NSString stringWithFormat:@"%lu",m_linesCount]];
+}
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
